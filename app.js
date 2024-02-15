eebe178b2018350b0b8ac5c202311452b1647284
vitaliypulupovuch <vitaliypulupovuch@gmail.com>
2024-02-15 20:15:52 +0100

    34:30

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-// переменние, которие могу
-// константи
+// Ctrl + K + C - закоментувати рядки
+// Ctrl + K + U - розкоментувати рядки
 
 let num = 42
-let firstName = 'Vitalii'
+let firstName = 'Vitalii' // стиль кемел кейс
 const isProgrammer = true
 
-// Can Do
+/* Can Do
+let $ = 'test'
+let $num = 42
+let num$ = 42
+let _ = 49
+let _num = 12
+let num_ = 12
+let first_name = 'Dilya' // bad погана практика
+let myNum = 34 // good
+let num42 = 10
+*/
+
+// Restricted - запрещено
+
