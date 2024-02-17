4c7d2e59af9df5f4637e45683eea482e78719d4a
vitaliypulupovuch <vitaliypulupovuch@gmail.com>
2024-02-17 20:11:44 +0100

    Operators

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // Ctrl + K + C - закоментувати рядки
 // Ctrl + K + U - розкоментувати рядки
 
-let num = 42
-let firstName = 'Vitalii' // стиль кемел кейс
-const isProgrammer = true
+let num = 42 // number
+let numFloat = 4.2 // float через точку
+let firstName = 'Vitalii' // string
+const isProgrammer = true // boolean
+// num - змінна - variable
+// firstName стиль кемел кейс CamelCase
 
-/* Can Do
+
+firstName = 'Max'
+
+/* Can Do -  разрешено
 let $ = 'test'
 let $num = 42
 let num$ = 42
@@ -17,5 +23,25 @@ let myNum = 34 // good
 let num42 = 10
 */
 
-// Restricted - запрещено
+/* Restricted - запрещено
+let 42num = '11' // початок цифри
+let my-num = 1 // через дефіс
+let false // зарезервовані слова
+*/
 
+// alert(firstName) // показує значення у випадаючому вікні
+// console.log('Test:', firstName, num, isProgrammer) // F12 - Console = має бути Test: Max
+
+// console.log(num + 10)
+// console.log(num - 10)
+// console.log(num * 10)
+// console.log(num / 10)
+// console.log(num)
+
+// let num2 = num + 10
+// console.log(num, num2)
+// num = num2 - num
+// console.log(num, num2)
+
+// let num3 =  (num + 10) * 2 / 3 -1
+// console.log(num3)
