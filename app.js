80ebcaa1ee6ea23b60332cad4af97e2f6d81279b
vitaliypulupovuch <vitaliypulupovuch@gmail.com>
2024-02-21 18:20:29 +0100

    Calculator 1 version

--- app.js
+++ app.js
@@ -54,6 +54,10 @@ let false // зарезервовані слова
 const resultElement = document.getElementById('result') // отримати елемент з HTML по id - це функція
 const input1 = document.getElementById('input1')
 const input2 = document.getElementById('input2')
+const submitBtn = document.getElementById('submit')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+let action = '+'
 
 // console.log(input2.value)
 // console.log(resultElement.textContent)
@@ -61,6 +65,44 @@ const input2 = document.getElementById('input2')
 
 // приведение типов, поміняти строчки на числа
 // const sum = input1.value + input2.value // сума складається не з чисел а з строк string 10 + 5 = 105
-const sum = Number(input1.value) + Number(input2.value)
-resultElement.textContent = sum 
-console.log(typeof sum) // перевірка типу typeof
+// console.log(typeof sum) // перевірка типу typeof
+
+plusBtn.onclick = function () {
+	action = '+'
+}
+
+minusBtn.onclick = function () {
+	action = '-'
+}
+
+function printResult (result) {
+	if (result < 0) {
+		resultElement.style.color = 'red'
+	} else {
+		resultElement.style.color = 'green'
+	}
+	resultElement.textContent = result
+}
+
+function computeNumbersWithAction (inp1, inp2, actionSymbol) {
+	const num1 = Number(inp1.value)
+	const num2 = Number(inp2.value)
+	if (actionSymbol == '+') {
+		return num1 + num2
+	}
+	if (actionSymbol == '-') {
+		return num1 - num2
+	}
+}
+
+submitBtn.onclick = function () { // додаємо обработчик собития onclick - event handler
+	const result = computeNumbersWithAction(input1, input2, action)
+	printResult(result)
+	// if (action == '+') {
+	// 	const sum = Number(input1.value) + Number(input2.value)
+	// 	printResult(sum)
+	// } else if (action == '-') {
+	// 	const sum = Number(input1.value) - Number(input2.value)
+	// 	printResult(sum)
+	// }
+}
