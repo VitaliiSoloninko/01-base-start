5383e74b28b8fb011b12bb0eeacd770defcd13fd
22051988Di <vitalii.soloninko@gmail.com>
2024-03-27 11:15:46 +0100

    debugger

--- app.js
+++ app.js
@@ -51,13 +51,13 @@ let false // зарезервовані слова
 
 /*--------------------------------------------------------------------------------------------------------*/
 
-const resultElement = document.getElementById('result'); // отримати елемент з HTML по id - це функція
-const input1 = document.getElementById('input1');
-const input2 = document.getElementById('input2');
-const submitBtn = document.getElementById('submit');
-const plusBtn = document.getElementById('plus');
-const minusBtn = document.getElementById('minus');
-let action = '+';
+const resultElement = document.getElementById('result') // отримати елемент з HTML по id - це функція
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const submitBtn = document.getElementById('submit')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+let action = '+'
 
 // console.log(input2.value)
 // console.log(resultElement.textContent)
@@ -68,43 +68,48 @@ let action = '+';
 // console.log(typeof sum) // перевірка типу typeof
 
 plusBtn.onclick = function () {
-  action = '+';
-};
+	action = '+'
+}
 
 minusBtn.onclick = function () {
-  action = '-';
-};
+	action = '-'
+}
 
 function printResult(result) {
-  if (result < 0) {
-    resultElement.style.color = 'red';
-  } else {
-    resultElement.style.color = 'green';
-  }
-  resultElement.textContent = result;
+	if (result < 0) {
+		resultElement.style.color = 'red'
+	} else {
+		resultElement.style.color = 'green'
+	}
+	resultElement.textContent = result
 }
 
 function computeNumbersWithAction(inp1, inp2, actionSymbol) {
-  const num1 = Number(inp1.value);
-  const num2 = Number(inp2.value);
-  if (actionSymbol == '+') {
-    return num1 + num2;
-  } else if (actionSymbol == '-') {
-    return num1 - num2;
-  }
-  // другий варіант запису, це тернарний оператор ? if : else
-  // return actionSymbol == '+' ? num1 + num2 : num1 - num2;
+	const num1 = +inp1.value
+	const num2 = +inp2.value
+	// +inp2.value Number(inp2.value) - перевести строчку в число 2 способа
+	// if (actionSymbol == '+') {
+	// 	return num1 + num2
+	// } else if (actionSymbol == '-') {
+	// 	return num1 - num2
+	// }
+
+	const result = actionSymbol == '+' ? num1 + num2 : num1 - num2
+	return result
+
+	// другий варіант запису, це тернарний оператор ? if : else
+	// return actionSymbol == '+' ? num1 + num2 : num1 - num2;
 }
 
 submitBtn.onclick = function () {
-  // додаємо обработчик собития onclick - event handler
-  const result = computeNumbersWithAction(input1, input2, action);
-  printResult(result);
-  // if (action == '+') {
-  // 	const sum = Number(input1.value) + Number(input2.value)
-  // 	printResult(sum)
-  // } else if (action == '-') {
-  // 	const sum = Number(input1.value) - Number(input2.value)
-  // 	printResult(sum)
-  // }
-};
+	// додаємо обработчик собития onclick - event handler
+	const result = computeNumbersWithAction(input1, input2, action)
+	printResult(result)
+	// if (action == '+') {
+	// 	const sum = Number(input1.value) + Number(input2.value)
+	// 	printResult(sum)
+	// } else if (action == '-') {
+	// 	const sum = Number(input1.value) - Number(input2.value)
+	// 	printResult(sum)
+	// }
+}
